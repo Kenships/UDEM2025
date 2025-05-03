31f88f313b3a7567ae7e6619d4b9ada3157b1a4b
Kenneth <kenships0402@gmail.com>
2025-05-03 15:01:08 -0400

    Make movement smoother

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,26 @@
 using System;
 using Obvious.Soap;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float turningSpeed = 10f;
     
     [SerializeField] private Vector2Variable movementInput;
 
+    [SerializeField] private GameObject rootVisual;
+
     private Vector2 direciton;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private void Start()
     {
         movementInput.OnValueChanged += OnMovementInputChanged;
+        rb = GetComponent<Rigidbody2D>();
+        sr = rootVisual.GetComponent<SpriteRenderer>();
     }
     private void OnMovementInputChanged(Vector2 direciton)
     {
@@ -20,6 +29,41 @@ public class PlayerMovement : MonoBehaviour
 
     public void Update()
     {
-        transform.position += (Vector3)direciton * (speed * Time.deltaTime);
+        Move();
+        OrientSprite();
+    }
+
+    private void OrientSprite()
+    {
+        if (rb.linearVelocity.x < 0)
+        {
+            sr.flipX = false;
+            
+        }
+        
+        if (rb.linearVelocity.x > 0)
+        {
+            sr.flipX = true;
+            
+        }
+
+        int rotation = sr.flipX ? 10 : -10;
+        if (rb.linearVelocity.y > 0)
+        {
+            rootVisual.transform.rotation = Quaternion.Lerp(rootVisual.transform.rotation, Quaternion.Euler(0, 0, rotation), Time.deltaTime* turningSpeed);
+        }
+        else if(rb.linearVelocity.y < 0)
+        {
+            rootVisual.transform.rotation = Quaternion.Lerp(rootVisual.transform.rotation, Quaternion.Euler(0, 0, -rotation), Time.deltaTime* turningSpeed);
+        }
+        else
+        {
+            rootVisual.transform.rotation = Quaternion.Lerp(rootVisual.transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime* turningSpeed);
+        }
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = (Vector3)direciton * speed;
     }
 }
