39af3076139bb8bd8c67a8ba5862a44e7b166291
Kenneth <kenships0402@gmail.com>
2025-05-03 22:17:35 -0400

    Oven Interaction and Visuals added

--- Assets/Scripts/Oven.cs
+++ Assets/Scripts/Oven.cs
@@ -1,15 +1,24 @@
 
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Oven : Appliance
 {
+    private List<GameObject> playersInteracting;
+
+    private void Awake()
+    {
+        playersInteracting = new List<GameObject>();
+    }
+
     public override void Interact(GameObject player)
     {
         PlayerInput playerInput = player.GetComponent<PlayerInput>();
         
         playerInput.SetUIMap();
         playerInput.cancelInput.OnRaised += Exit;
-
+    
         if (playerInput.GetPlayerNumber() == 1)
         {
             Instantiate(popupWindow, PopupManager.Instance.leftPopup.transform);
@@ -18,6 +27,10 @@ public class Oven : Appliance
         {
             Instantiate(popupWindow, PopupManager.Instance.rightPopup.transform);
         }
+        
+        
+        playersInteracting.Add(player);
+        activeVisual.SetActive(true);
     }
 
     public override void Exit(GameObject player)
@@ -33,6 +46,13 @@ public class Oven : Appliance
         else if(playerInput.GetPlayerNumber() == 2)
         {
             Destroy(PopupManager.Instance.rightPopup.transform.GetChild(0).gameObject);
+        }
+
+        playersInteracting.Remove(player);
+
+        if (playersInteracting.Count == 0)
+        {
+            activeVisual.SetActive(false);
         }
     }
     
